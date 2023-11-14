c62c68cbca5c9fa9a27333771492000a62b0df17
cesar <52505329+servercesar@users.noreply.github.com>
2023-11-14 09:43:43 -0300

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(sum(1, 2), 3)
+        self.assertEqual(sum([1, 2]), 3)
 
 if __name__ == '__main__':
     unittest.main()
